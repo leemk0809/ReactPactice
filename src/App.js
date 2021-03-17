376737195fee5debd68fcf586d2a20984983fe66
leemk <qkqhsj34@naver.com>
2021-03-17 18:01:55 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   
-  let posts = 'title';
+  //ES6 destructuring 文法
+  let [title,titleChange] = useState(['男性のコートおすすめ！'],　['女性のコートおすすめ！']);
+  //let [title,titleChange] = useState('女性のコートおすすめ！');
 
+  let posts = 'title';
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ {color : 'blue', fontSize: '30px'} }>開発ブログ</div>
+        <div>開発ブログ</div>
       </div>
-      <h4>{ posts }</h4>
+      <div className="list"> 
+        <h3> { title[1] } </h3>
+        <p>2月17日発行</p>
+        <p>asdasdasdasdasd</p>
+        <hr/>
+      </div>
     </div>
   );
 }
